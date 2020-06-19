b9d65de891adc36a6d81af2830af71ee819c5e49
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-06-19 12:39:35 -0500

    robot sort complete

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,30 +81,85 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
         """
         return self._light == "ON"
 
+# UPER
+
+# UNDERSTAND:
+# We have a robot with limited capabilities. It can move left or right in a single
+# direction, pick up an item which requires swapping its currently held item with the
+# one it wants to pick up in front of it, compare its currently held item to the item
+# in front of it, and switch a light on its head on or off.
+
+# We must use these limited capabilities to sort a list that the robot receives on
+# instantiation of the class when it's created.
+
+# Some rules we have to adhear by is not modifying any pre-defined methods that the robot
+# already has, not creating any new variables to store information, not access any instance
+# variables directly (self._anything), and not using any Python libraries or class methods to
+# make the sorting of the list easy / automatic
+
+# We can however create / define robot helper methods as long as they follow all the rules.
+
+# PLAN:
+# We learned about the bubble sort, and I think that this would be a great sorting algorithm
+# to use for this problem. Because the robot can move incrementally through the list and compare
+# values and turn his light on or off, we can move through the list and perform a similar, but
+# different swap in relation to a bubble sort. It's different because the robot does not swap the
+# two values next to each other, but instead, swaps its current item with the current item in front
+# of it. The robot also starts off holding 'None', so the there will be one 'None' value floating
+# around in the list, which differs from a traditional bubble sort.
+
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        self.set_light_on()
+        while self.light_is_on():
+            # our break case
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                # next item is smaller swap it with current item and move left
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                else:  # item is larger
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+            if self.light_is_on == False:
+                return
+            while self.can_move_left():
+                self.move_left()
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    # small_list = [5, 4, 3, 2, 1]
+    # [5, None, 2, 1, 4]
+    # Holding: 3
+    # Index: [1]
+    # i = 1 -> going to go to 4
 
     robot = SortingRobot(l)
 
